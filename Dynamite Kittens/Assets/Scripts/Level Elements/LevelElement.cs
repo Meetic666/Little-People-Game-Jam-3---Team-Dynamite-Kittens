8962a2478c33a65602fd70dc1c2835990f0fb712
iwan0008 <iwan0008@algonquinlive.com>
2014-12-12 23:50:11 -0500

    Changes: - Added more functionality to the level elements base class

--- Dynamite Kittens/Assets/Scripts/Level Elements/LevelElement.cs
+++ Dynamite Kittens/Assets/Scripts/Level Elements/LevelElement.cs
@@ -1,14 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LevelElement : MonoBehaviour 
 {
+    protected enum EmotionalState
+    {
+        Happy,
+        Ecstatic,
+        Insane
+    };
+
     public bool EnemyCanInteractWith = true;
     public bool PlayerCanInteractWith = true;
+    public bool GoreCanInteractWith = true;
+
+    private EmotionalState emotionalState = EmotionalState.Happy;
+
+    private List<GameObject> InteractingObjects = new List<GameObject>();
 
     private void InteractWith(PlayerMovement player)
     {
-        //VirtualInteractWith(player);
+        VirtualInteractWith(player);
+    }
+
+    private void StopInteractWith(PlayerMovement player)
+    {
+        VirtualStopInteractWith(player);
+    }
+
+    private void InteractWith(GameObject gore)
+    {
+        VirtualInteractWith(gore);
+    }
+
+    private void StopInteractWith(GameObject gore)
+    {
+        VirtualStopInteractWith(gore);
     }
 
     //private void InteractWith(Enemy enemy)
@@ -16,12 +44,68 @@ public class LevelElement : MonoBehaviour
     //    VirtualInteractWith(enemy);
     //}
 
-    //protected void VirtualInteractWith(PlayerMovement player);
-    //protected void VirtualInteractWith(Enemy enemy);
+    //private void StopInteractWith(Enemy enemy)
+    //{
+    //    VirtualStopInteractWith(enemy);
+    //}
 
-    protected void SwitchTexture()
+    protected virtual void VirtualInteractWith(PlayerMovement player)
     {
 
+    }
+
+    protected virtual void VirtualStopInteractWith(PlayerMovement player)
+    {
+
+    }
+
+    protected virtual void VirtualInteractWith(GameObject gore)
+    {
+
+    }
+
+    protected virtual void VirtualStopInteractWith(GameObject gore)
+    {
+
+    }
+
+    //protected virtual void VirtualInteractWith(Enemy enemy)
+    //{
+
+    //}
+
+    //protected virtual void VirtualStopInteractWith(Enemy enemy)
+    //{
+
+    //}
+
+    protected virtual void PlayAnimation(EmotionalState state)
+    {
+
+    }
+
+    protected virtual void ChangeState(EmotionalState state)
+    {
+        if (emotionalState == EmotionalState.Insane ||
+            emotionalState == state) { return; }
+
+        emotionalState = state;
+    }
+
+    private void AddObjectToList(GameObject obj)
+    {
+        InteractingObjects.Add(obj);
+    }
+
+    private void RemoveObjectFromList(GameObject obj)
+    {
+        foreach (GameObject interactingObject in InteractingObjects)
+        {
+            if (interactingObject == obj)
+            {
+                InteractingObjects.Remove(obj);
+            }
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -32,7 +116,34 @@ public class LevelElement : MonoBehaviour
 
             if (player != null)
             {
+                ChangeState(EmotionalState.Ecstatic);
+                AddObjectToList(player.gameObject);
                 InteractWith(player);
+            }
+        }
+
+        if (collision.gameObject.tag == "Enemy" && EnemyCanInteractWith == true)
+        {
+            ChangeState(EmotionalState.Ecstatic);
+        }
+
+        if (collision.gameObject.tag == "Gore" && GoreCanInteractWith == true)
+        {
+            ChangeState(EmotionalState.Insane);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && PlayerCanInteractWith == true)
+        {
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+
+            if (player != null)
+            {
+                RemoveObjectFromList(player.gameObject);
+                if (InteractingObjects.Count == 0) { ChangeState(EmotionalState.Happy); }
+                StopInteractWith(player);
             }
         }
 
