0d675a47fe4f6ccc67bdb59b49928ccdcb97a2fb
iwan0008 <iwan0008@algonquinlive.com>
2014-12-12 21:37:48 -0500

    Changes: - Created a test Scene - Added a level elements base class (WIP)

---
+++ Dynamite Kittens/Assets/Scripts/Level Elements/LevelElement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelElement : MonoBehaviour 
+{
+    public bool EnemyCanInteractWith = true;
+    public bool PlayerCanInteractWith = true;
+
+    private void InteractWith(PlayerMovement player)
+    {
+        VirtualInteractWith(player);
+    }
+
+    //private void InteractWith(Enemy enemy)
+    //{
+    //    VirtualInteractWith(enemy);
+    //}
+
+    protected void VirtualInteractWith(PlayerMovement player);
+    //protected void VirtualInteractWith(Enemy enemy);
+
+    protected void SwitchTexture()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && PlayerCanInteractWith == true)
+        {
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+
+            if (player != null)
+            {
+                InteractWith(player);
+            }
+        }
+
+        if (collision.gameObject.tag == "Enemy" && EnemyCanInteractWith == true)
+        {
+
+        }
+    }
+}
