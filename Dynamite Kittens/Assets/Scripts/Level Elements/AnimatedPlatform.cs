68cfe694fbc1d86b5315b1d7d67ee7c557633e65
Meetic666 <bellay.quentin@gmqil.com>
2014-12-13 01:37:14 -0500

    Created platform prefabs

---
+++ Dynamite Kittens/Assets/Scripts/Level Elements/AnimatedPlatform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedPlatform : MonoBehaviour 
+{
+	public bool m_IsEye;
+	public bool m_IsMouth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Animator>().SetBool("IsEye", m_IsEye);
+		GetComponent<Animator>().SetBool("IsMouth", m_IsMouth);
+	}
+}
