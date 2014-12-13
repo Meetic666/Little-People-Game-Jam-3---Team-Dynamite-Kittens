225a5a7da5241f0d76140cc2bb42a0635f0116e3
iwan0008 <iwan0008@algonquinlive.com>
2014-12-13 14:14:14 -0500

    Changes: - Added functionality to level elements base class - Added platform script - Set up test scenario for platform emotional state

--- Dynamite Kittens/Assets/Scripts/Level Elements/LevelElement.cs
+++ Dynamite Kittens/Assets/Scripts/Level Elements/LevelElement.cs
@@ -6,38 +6,45 @@ public class LevelElement : MonoBehaviour
 {
     protected enum EmotionalState
     {
-        Happy,
+        Happy = 0,
         Ecstatic,
-        Insane
+        Insane,
+        None
     };
 
     public bool EnemyCanInteractWith = true;
     public bool PlayerCanInteractWith = true;
     public bool GoreCanInteractWith = true;
 
-    private EmotionalState emotionalState = EmotionalState.Happy;
+    private Animator m_Animator;
+    private EmotionalState m_EmotionalState = EmotionalState.Happy;
+    private List<GameObject> m_InteractingObjects = new List<GameObject>();
+    private Animator[] m_AnimatedSprites;
 
-    private List<GameObject> InteractingObjects = new List<GameObject>();
+    void Awake()
+    {
+        m_AnimatedSprites = GetComponentsInChildren<Animator>();
+    }
 
-    private void InteractWith(PlayerMovement player)
+    private void InteractWith(GameObject player)
     {
         VirtualInteractWith(player);
     }
 
-    private void StopInteractWith(PlayerMovement player)
+    private void StopInteractWith(GameObject player)
     {
         VirtualStopInteractWith(player);
     }
 
-    private void InteractWith(GameObject gore)
-    {
-        VirtualInteractWith(gore);
-    }
+    //private void InteractWith(GameObject gore)
+    //{
+    //    VirtualInteractWith(gore);
+    //}
 
-    private void StopInteractWith(GameObject gore)
-    {
-        VirtualStopInteractWith(gore);
-    }
+    //private void StopInteractWith(GameObject gore)
+    //{
+    //    VirtualStopInteractWith(gore);
+    //}
 
     //private void InteractWith(Enemy enemy)
     //{
@@ -49,25 +56,25 @@ public class LevelElement : MonoBehaviour
     //    VirtualStopInteractWith(enemy);
     //}
 
-    protected virtual void VirtualInteractWith(PlayerMovement player)
+    protected virtual void VirtualInteractWith(GameObject player)
     {
 
     }
 
-    protected virtual void VirtualStopInteractWith(PlayerMovement player)
+    protected virtual void VirtualStopInteractWith(GameObject player)
     {
 
     }
 
-    protected virtual void VirtualInteractWith(GameObject gore)
-    {
+    //protected virtual void VirtualInteractWith(GameObject gore)
+    //{
 
-    }
+    //}
 
-    protected virtual void VirtualStopInteractWith(GameObject gore)
-    {
+    //protected virtual void VirtualStopInteractWith(GameObject gore)
+    //{
 
-    }
+    //}
 
     //protected virtual void VirtualInteractWith(Enemy enemy)
     //{
@@ -81,38 +88,65 @@ public class LevelElement : MonoBehaviour
 
     protected virtual void PlayAnimation(EmotionalState state)
     {
-
+        switch (state)
+        {
+            case EmotionalState.Happy:
+                for (int i = 0; i < m_AnimatedSprites.Length; i++)
+                {
+                    m_AnimatedSprites[i].SetBool("IsEcstatic", false);
+                    m_AnimatedSprites[i].SetBool("IsInsane", false);
+                }
+                break;
+            case EmotionalState.Ecstatic:
+                for (int i = 0; i < m_AnimatedSprites.Length; i++)
+                {
+                    m_AnimatedSprites[i].SetBool("IsEcstatic", true);
+                    m_AnimatedSprites[i].SetBool("IsInsane", false);
+                }
+                break;
+            case EmotionalState.Insane:
+                for (int i = 0; i < m_AnimatedSprites.Length; i++)
+                {
+                    m_AnimatedSprites[i].SetBool("IsEcstatic", false);
+                    m_AnimatedSprites[i].SetBool("IsInsane", true);
+                }
+                break;
+            case EmotionalState.None:
+                break;
+        }
     }
 
     protected virtual void ChangeState(EmotionalState state)
     {
-        if (emotionalState == EmotionalState.Insane ||
-            emotionalState == state) { return; }
+        if (m_EmotionalState == EmotionalState.Insane ||
+            m_EmotionalState == state) { return; }
 
-        emotionalState = state;
+        m_EmotionalState = state;
+        PlayAnimation(state);
     }
 
     private void AddObjectToList(GameObject obj)
     {
-        InteractingObjects.Add(obj);
+        m_InteractingObjects.Add(obj);
     }
 
     private void RemoveObjectFromList(GameObject obj)
     {
-        foreach (GameObject interactingObject in InteractingObjects)
+        foreach (GameObject interactingObject in m_InteractingObjects)
         {
             if (interactingObject == obj)
             {
-                InteractingObjects.Remove(obj);
+                m_InteractingObjects.Remove(obj);
             }
         }
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && PlayerCanInteractWith == true)
         {
-            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+            //PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+            GameObject player = collision.gameObject;
 
             if (player != null)
             {
@@ -133,16 +167,17 @@ public class LevelElement : MonoBehaviour
         }
     }
 
-    void OnCollisionExit(Collision collision)
+    void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && PlayerCanInteractWith == true)
         {
-            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+            //PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+            GameObject player = collision.gameObject;
 
             if (player != null)
             {
                 RemoveObjectFromList(player.gameObject);
-                if (InteractingObjects.Count == 0) { ChangeState(EmotionalState.Happy); }
+                if (m_InteractingObjects.Count == 0) { ChangeState(EmotionalState.Happy); }
                 StopInteractWith(player);
             }
         }
---
+++ Dynamite Kittens/Assets/Scripts/Level Elements/Platform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : LevelElement 
+{
+	void Start () 
+    {
+        PlayerCanInteractWith = true;
+        EnemyCanInteractWith = true;
+        GoreCanInteractWith = true;
+	}
+}
