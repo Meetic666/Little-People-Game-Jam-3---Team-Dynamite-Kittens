0d00c2778f5bfbf0d960b5d23924ff22aceb27ac
Meetic666 <bellay.quentin@gmqil.com>
2014-12-14 15:41:11 -0500

    Added Splash Sreen (still need Kezia's art)

---
+++ Dynamite Kittens/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Attack"))
+		{
+			Application.LoadLevel("Main");
+		}
+	}
+}
