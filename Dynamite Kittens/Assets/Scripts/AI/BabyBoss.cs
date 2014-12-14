16e036f8b63a70bec25b8c4beb4d97da89255597
pryd0008 <pryd0008@algonquinlive.com>
2014-12-14 12:52:34 -0500

    - Removed limb joints, rotation speed and bobbing speed of boss

--- Dynamite Kittens/Assets/Scripts/AI/BabyBoss.cs
+++ Dynamite Kittens/Assets/Scripts/AI/BabyBoss.cs
@@ -12,76 +12,46 @@ public class BabyBoss : MonoBehaviour
 	public float m_SpawnIntervalTime;
 	float m_SpawnIntervalTimer;
 	
-	public float m_JointRotationSpeed;
-	float m_CurrentJointRotationSpeed;
-
-	public float m_BobbingSpeed;
-	float m_CurrentBobbingSpeed;
-	
-	//Limb Joints
-	public GameObject m_LeftArm;
-	public GameObject m_RightArm;
-	
 	//Enemies to spawn
 	public GameObject[] m_Enemies = new GameObject[3];
 	
 	public GameObject m_SpawnPoint;
+	public int m_NumberOfEnemiesPerWave;
 
 	// Use this for initialization
 	void Start () 
 	{
 		m_SpawnIntervalTimer = m_SpawnIntervalTime;
-		m_CurrentJointRotationSpeed = m_JointRotationSpeed;
-		m_CurrentBobbingSpeed = m_BobbingSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if(m_CurrentJointRotationSpeed > 0)
+		if(m_SpawnIntervalTimer <= 0)
 		{
-			if(m_LeftArmUp)
+			for(int i = 0; i < m_NumberOfEnemiesPerWave; i++)
 			{
-				m_LeftArm.transform.Rotate(transform.forward, m_CurrentJointRotationSpeed);
-				m_RightArm.transform.Rotate(transform.forward, -m_CurrentJointRotationSpeed);
-			}
-			else
-			{
-				m_LeftArm.transform.Rotate(transform.forward, -m_CurrentJointRotationSpeed);
-				m_RightArm.transform.Rotate(transform.forward, m_CurrentJointRotationSpeed);
+				int rand = Random.Range(0, 3);
+				GameObject enemy = (GameObject)Instantiate(m_Enemies[rand], m_SpawnPoint.transform.position, Quaternion.identity);
+				
+				if(i == 2)
+				{
+					enemy.transform.Rotate(enemy.transform.up, 180);
+				}
 			}
 
-			m_CurrentJointRotationSpeed -= Time.deltaTime;
+			m_SpawnIntervalTimer = m_SpawnIntervalTime;
 		}
 		else
 		{
-			m_CurrentJointRotationSpeed = m_JointRotationSpeed;
-			m_LeftArmUp = !m_LeftArmUp;
-		}
-
-		if(m_CurrentBobbingSpeed > 0)
-		{
-			if(m_BobbingUp)
-			{
-				transform.position += transform.up * m_CurrentBobbingSpeed;
-			}
-			else
-			{
-				transform.position += -transform.up * m_CurrentBobbingSpeed;
-			}
-			m_CurrentBobbingSpeed -= Time.deltaTime;
-		}
-		else
-		{
-			m_BobbingUp = !m_BobbingUp;
-			m_CurrentBobbingSpeed = m_BobbingSpeed;
+			m_SpawnIntervalTimer -= Time.deltaTime;
 		}
 	}
 
 	public void Damage()
 	{
 		m_Health--;
-		//TODO: Detach limbs until dead
+		//TODO: Update animator
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
