47975795be48d38b71270f4cb289addde11257f6
Meetic666 <bellay.quentin@gmqil.com>
2014-12-14 14:57:11 -0500

    Added hover effect to boots
    Added boots in level

--- Dynamite Kittens/Assets/Scripts/Pick Ups/BootPickUp.cs
+++ Dynamite Kittens/Assets/Scripts/Pick Ups/BootPickUp.cs
@@ -5,6 +5,27 @@ public class BootPickUp : MonoBehaviour
 {
 	public int m_PickUpDamageUpgrade;
 
+	public float m_WaveAmplitude;
+	public float m_WaveSpeed;
+	
+	float m_WaveCenterY;
+	
+	float m_Time;
+	
+	void Start()
+	{
+		m_WaveCenterY = transform.position.y;
+	}
+	
+	void Update()
+	{
+		m_Time += Time.deltaTime * m_WaveSpeed;
+		
+		Vector3 newPosition = transform.position;
+		newPosition.y = m_WaveCenterY + m_WaveAmplitude * Mathf.Cos(m_Time);
+		transform.position = newPosition;
+	}
+
 	void OnTriggerEnter2D(Collider2D collider)
 	{
 		PlayerAttack player = collider.GetComponent<PlayerAttack>();
